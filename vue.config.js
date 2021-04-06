b562793bd0c833d63e75ad3b5b0af0fcff2b3037
gromit <1522757361@qq.com>
2021-04-06 14:13:25 +0800

    add and set vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === "production") {
+      // 为生产环境修改配置...
+    } else {
+      // 为开发环境修改配置...
+
+      // 只修改开发环境配置，打包会出现找不到定义的别名的错误
+		return {
+
+		};
+    }
+  }
+};
